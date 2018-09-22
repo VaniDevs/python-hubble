1dd74bee86c7e169d8d8dca3dbb9ed2daa3eddcb
Glen Pierce <glen@jostle.me>
2018-09-22 12:40:03 -0700

    s3-integration prototype

--- hubble/app.py
+++ hubble/app.py
@@ -1,4 +1,5 @@
 import falcon
+import os
 from hubble import resources, models
 
 
@@ -7,4 +8,9 @@ def create_app(config):
 
     app = falcon.API()
     app.add_route('/events', resources.Events())
+    hello_world = resources.HelloWorld()
+    send_update = resources.SendUpdate()
+    app.add_route('/hello', hello_world)
+    app.add_route('/hello/{key}', hello_world)
+    app.add_route('/send_update', send_update)
     return app
--- hubble/resources.py
+++ hubble/resources.py
@@ -1,6 +1,16 @@
 import falcon
 import json
+import boto3
+import time
 
+from hubble.models import Event
+
+s3 = boto3.resource('s3', region_name='ca-central-1')
+
+bucketName = "vanhacks2018"
+key = "uploadFileName"
+outputName = int(round(time.time() * 1000))
+# s3.put_object(Bucket=bucketName, Key=key, Body=b'foobar')
 
 class HelloWorld:
     def on_get(self, req, resp, key=None):
@@ -8,3 +18,9 @@ class HelloWorld:
             resp.media = json.dumps({'hello': 'world'})
         else:
             resp.media = json.dumps({'hello': key})
+
+class SendUpdate:
+    def on_post(self, req, resp):
+        s3.put_object(Bucket=bucketName, Key=key, Body=b'foobar')
+        event = Event(req.img_data, req.lat, req.long, req.comments)
+        event.save()
