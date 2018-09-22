215b466260f85b1d9c3e693c4eaced592013991f
Demian Brecht <demianbrecht@gmail.com>
2018-09-22 10:52:52 -0700

    first models commit

--- hubble/models.py
+++ hubble/models.py
@@ -1,27 +1,43 @@
 import json
 
 
-class Model:
-    def to_dict(self):
-        return self.__dict__
+class Event:
+    def __init__(self, img_data, lat, long, comments=None):
+        self.img_data = img_data
+        self.lat = lat
+        self.long = long
+        self.comments = comments
 
-
-class User(Model):
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
+    def save(self):
+        # TODO: wire up to db
+        _EVENTS.append(self)
 
     @staticmethod
     def all():
-        return _USERS.values()
-
-    @staticmethod
-    def get(key):
-        return _USERS[key]
+        # TODO: wire up to db
+        return _EVENTS 
 
 
-# just for testing
-_USERS = {
-    'foo': User('foo', 'foo@baz.com'),
-    'bar': User('bar', 'bar@baz.com'),
-}
+def _generate_debug_data():
+    import random, base64
+
+    with open('data/meme.jpg', 'rb') as test_img:
+        img_data = base64.b64encode(test_img.read())
+
+    events = []
+
+    for _ in range(100):
+        lat = random.uniform(-90, 90)
+        long = random.uniform(-180, 180)
+
+        events.append(
+            Event(
+                random.uniform(-90, 90),
+                random.uniform(-90, 90),
+                'someting bad happened'
+            )
+        )
+
+    return events
+
+_EVENTS = _generate_debug_data()
