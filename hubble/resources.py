d03d6cf702f156f3ec031fdf4092e040d2f2de34
Demian Brecht <demianbrecht@gmail.com>
2018-09-22 14:07:42 -0700

    events get endpoint

--- hubble/resources.py
+++ hubble/resources.py
@@ -1,10 +1,33 @@
 import falcon
 import json
 
+from hubble import models
 
-class HelloWorld:
-    def on_get(self, req, resp, key=None):
-        if key is None:
-            resp.media = json.dumps({'hello': 'world'})
-        else:
-            resp.media = json.dumps({'hello': key})
+
+
+class Event:
+    def on_get(self, req, resp):
+        filters = req.params
+        events = models.Event.all(
+            offset=filters.get('offset'),
+            limit=filters.get('limit')
+        )
+        resp.media = [
+            {
+                'id': str(ev.id),
+                'image_url': ev.image_url,
+                'comments': ev.comments,
+                'location': {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [ev.longitude, ev.lattitude],
+                    },
+                    "properties": {
+                        "name": ev.location_name,
+                    }
+                },
+                'created': ev.created,
+            }
+            for ev in events
+        ]
