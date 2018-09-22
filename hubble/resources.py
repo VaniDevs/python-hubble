fc30b903561e71e96f50df2393a2902085f8ac38
Glen Pierce <glen@jostle.me>
2018-09-22 13:49:19 -0700

    upload image to s3 and save the event

--- hubble/resources.py
+++ hubble/resources.py
@@ -2,15 +2,18 @@ import falcon
 import json
 import boto3
 import time
+import uuid
 
 from hubble.models import Event
 
-s3 = boto3.resource('s3', region_name='ca-central-1')
+s3_region_name = 'ca-central-1'
 
-bucketName = "vanhacks2018"
-key = "uploadFileName"
-outputName = int(round(time.time() * 1000))
+s3 = boto3.resource('s3', region_name=s3_region_name)
+
+bucket_name = "vanhacks2018"
+key = str(uuid.uuid4())
 # s3.put_object(Bucket=bucketName, Key=key, Body=b'foobar')
+
 
 class HelloWorld:
     def on_get(self, req, resp, key=None):
@@ -19,8 +22,13 @@ class HelloWorld:
         else:
             resp.media = json.dumps({'hello': key})
 
+
 class SendUpdate:
     def on_post(self, req, resp):
-        s3.put_object(Bucket=bucketName, Key=key, Body=b'foobar')
-        event = Event(req.img_data, req.lat, req.long, req.comments)
+        print("saving key " + key)
+        s3.put_object(Bucket=bucket_name, Key=key, Body=b'foobar')
+        print("Getting S3 object " + key)
+        response = s3.get_object(Bucket=bucket_name, Key=key)
+        image_url = "http://s3-" + s3_region_name + ".amazonaws.com/" + bucket_name + "/" + key
+        event = Event(image_url, req.lat, req.long, req.comments)
         event.save()
